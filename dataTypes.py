e62c90852c95ef20e770c1dcd17f45dd4b972e06
Kaan ARI <kaanari@hacettepe.edu.tr>
2020-10-01 13:48:38 +0300

    Family class is added

--- dataTypes.py
+++ dataTypes.py
@@ -1,5 +1,6 @@
 import datetime
 
+
 class Person:
     id = 0
 
@@ -13,6 +14,8 @@ class Person:
 
         self.isAlive = True
 
+        self.isMarried = False
+
         Person.id += 1
 
     def kill(self):
@@ -25,3 +28,37 @@ class Person:
 
     def calculateAge(self):
         self.age = datetime.datetime.now().year - self.birthDate.year
+
+    def __add__(self, other):
+        if (not self.isMarried) and (not other.isMarried):
+            return Family(self,other)
+        else:
+            return None
+
+
+class Family:
+    id = 0
+
+    def __init__(self, groom, bride):
+        self.id = Family.id
+        Family.id += 1
+
+        self.children = []
+        self.woman = bride
+        self.man = groom
+
+        self.woman.isMarried = True
+        self.man.isMarried = True
+
+    def addChild(self, name):
+        child = Person(name)
+        self.children.append(child)
+
+
+P1 = Person("John")
+P2 = Person("Claire")
+
+F1 = Family(P1, P2)
+F2 = P1 + P2
+
+
